8aa9ab91d18f77e851f416b69da49b488c76d4ac
403 <403@DESKTOP-U7F1UNK>
2018-09-11 16:44:58 +0900

    java

--- src/day02/Hello.java
+++ src/day02/Hello.java
@@ -1,10 +1,17 @@
 package day02;
-
+/*
+ * 여러줄의 주석표현
+ */
+/**
+ * 작성일 : 2018-09-11
+ * 작성자 : 서지우
+ * 설명 : 1주차 자바 프로그래밍 연습
+ */
 public class Hello {
-
+	// 한줄 주석표현
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
-		// 컴파일 ctrl+F11
+		// 컴파일 ctrl+F11 
 		
 	}
 
