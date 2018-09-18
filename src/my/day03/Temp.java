99d6f9e74970202e20d9f8c9eb2b769f339fe154
403 <403@DESKTOP-U7F1UNK>
2018-09-18 17:35:53 +0900

    java

--- src/my/day03/Temp.java
+++ src/my/day03/Temp.java
@@ -9,7 +9,7 @@ public class Temp {
 		System.out.print("화씨온도를 입력하시오: ");
 		f=input.nextDouble();
 		
-		c=5.0/9.0*(f-32.0);
+		c=5.0/9.0*(f-32.0); // double형이니 소수점까지 표현해야함
 		
 		System.out.println("섭씨온도는"+c);
 	}
